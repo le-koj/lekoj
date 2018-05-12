28fcf4369d6b233a616d749e2bd68409671aa3bd
le koj <mitchkojelegance@gmail.com>
2018-05-11 23:08:57 -0700

    pictures added to root folder

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import webapp2, os, jinja2, model, Picturefile, logging
-import cloudstorage, mimetypes
+import cloudstorage as gcs
+import mimetypes
 from google.appengine.api import app_identity
-#from google import cloud
+from google.appengine.ext import blobstore
+from google.appengine.ext.webapp import blobstore_handlers
 
 
 
@@ -11,6 +13,10 @@ template_dir = os.path.join(os.path.dirname(__file__))
 # jinja environment instance to load html templates
 jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(template_dir), autoescape=True)
+
+# list of image names
+images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg',
+          '8.jpg', '9.jpg', '10.jpg']
 
 class MainHandler(webapp2.RequestHandler):
     """main object to handle index.html request"""
@@ -23,11 +29,22 @@ class MainHandler(webapp2.RequestHandler):
         rendered_template = self._render_template('index.html', context=context)
 
         #>>>>>>>>>>>>>> code test <<<<<<<<<<<<<<<<<<<<#
+        # get cloud storage bucket
+        bucket_name = os.environ.get('BUCKET_NAME',
+                                    app_identity.get_default_gcs_bucket_name())
+
+        # collect file from bucket
+        real_path = '/' + bucket_name + '/1.jpg'
+        #self.createMultiFiles(bucket_name, images)
+
+        # read images from bucket
+        image_file = self.readFile(real_path)
+
         """storage_client = cloud.client.Client()
         bucket_name = 'shining-axon-201518.appspot.com'
         bucket = storage_client.get_bucket(bucket_name)
         blobs = bucket.list_blobs()
-        self.response.out.write(blobs)"""
+        self.response.out.write(blobs)
 
         bucket_name = app_identity.get_default_gcs_bucket_name()
         print(bucket_name)
@@ -36,7 +53,7 @@ class MainHandler(webapp2.RequestHandler):
         #real_path = os.path.join('/',bucket_name,'text_file.txt')
         #real_path = '/shining-axon-201518.appspot.com'
 
-        """
+
         try:
             with cloudstorage.open(real_path, 'r') as f:
                 self.response.out.write(f.read())
@@ -57,6 +74,7 @@ class MainHandler(webapp2.RequestHandler):
 
         # send out rendered template
         self.response.out.write(rendered_template)
+        self.response.out.write(image_file)
 
 
     # method to final template for response
@@ -82,6 +100,75 @@ class MainHandler(webapp2.RequestHandler):
 
         context['category'] = _categories
         return context
+
+    # method to read files from cloudstorage bucket
+    def readFile(self, filename):
+        self.response.write('Reading the full file contents:\n')
+
+        # create blobstore filename
+        blobstore_filename = '/gs{}'.format(filename)
+
+        # create blob key
+        blob_key = blobstore.create_gs_key(blobstore_filename)
+
+        contents = blobstore.fetch_data(blob_key, 0, 1)
+        """
+        gcs_file = gcs.open(filename, 'r') ## open file in bucket
+        contents = gcs_file.read()    ## read openned file
+        gcs_file.close()              ## close file
+
+
+
+        try:
+            with gcs.open(filename, 'r') as f:
+                self.response.out.write(f.read())
+        except gcs.errors.NotFoundError:
+                self.abort(404)
+        """
+        return contents
+
+
+    # method to write to bucket
+    def createFile(self, filename, local_file):
+         """Create a file.
+
+          The retry_params specified in the open call will override the default
+          retry params for this particular file handle.
+
+          Args:
+            filename: filename.
+         """
+         self.response.write('Creating file %s\n' % filename)
+
+         image = self.get_local_file(local_file)
+
+         write_retry_params = gcs.RetryParams(backoff_factor=1.1)
+         gcs_file = gcs.open(filename,
+                             'w',
+                             content_type='image/jpg',
+                             retry_params=write_retry_params)
+         gcs_file.write(image)
+         #gcs_file.write('f'*1024*4 + '\n')
+         gcs_file.close()
+
+
+
+    # method to create multiple files in cloudstorage bucket
+    def createMultiFiles(self, bucket_name, arr):
+
+        for i in arr:
+            real_path = '/' + bucket_name + '/' + i
+            self.createFile(real_path, i)
+
+
+    # method to return local file
+    def get_local_file(self, filename):
+
+        with open(filename, 'rb') as f:
+            jpg_data = f.read()
+
+        return jpg_data
+
 
 
 class AboutLinkHandler(MainHandler):
